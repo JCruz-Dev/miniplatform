be38c6da3b5360d228bde7621aaec90d08541db0
Juan carlos <juancarloscruz278@gmail.com>
2021-01-16 21:17:22 -0400

    feature: creates Logo component

---
+++ components/Logo.vue
@@ -0,0 +1,15 @@
+<template>
+  <img
+    src="~/assets/Icons/logo.svg"
+    alt="Talently Logo"
+    class="items-start block form__content__img"
+  />
+</template>
+
+<script>
+export default {
+  name: 'Logo',
+}
+</script>
+
+<style></style>
