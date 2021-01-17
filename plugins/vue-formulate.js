60d8a1e908a90e74044b3520c94030fd289d43ad
Juan carlos <juancarloscruz278@gmail.com>
2021-01-16 21:17:05 -0400

    feature: adds vue formulate to handle forms

---
+++ plugins/vue-formulate.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import VueFormulate from '@braid/vue-formulate'
+import { es } from '@braid/vue-formulate-i18n'
+
+Vue.use(VueFormulate, {
+  plugins: [es],
+  locale: 'es',
+})
